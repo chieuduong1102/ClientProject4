2928b6ea0fa18a8d361ea9890bb8293b1217383e
Duong Pham Huu <chieuduong1102@gmail.com>
2021-09-06 21:06:17 +0700

    Update ProductDetail

--- src/components/AdminComponnents/ProductDetail.vue
+++ src/components/AdminComponnents/ProductDetail.vue
@@ -6,33 +6,63 @@
         <h1>Information</h1>
       </div>
       <div class="col-md-6">
-        <br>
+        <br />
         <div class="row">
-            <div class="col-md-4"><img src="../../assets/css/img/product/pr1.png" style="width: 150px; height: 150px;" alt=""></div>
-            <div class="col-md-4"><img src="../../assets/css/img/product/pr1.png" style="width: 150px; height: 150px;" alt=""></div>
-            <div class="col-md-4"><img src="../../assets/css/img/product/pr1.png" style="width: 150px; height: 150px;" alt=""></div>
+          <div class="col-md-4">
+            <img
+              src="../../assets/css/img/product/pr1.png"
+              style="width: 150px; height: 150px"
+              alt=""
+            />
+          </div>
+          <div class="col-md-4">
+            <img
+              src="../../assets/css/img/product/pr1.png"
+              style="width: 150px; height: 150px"
+              alt=""
+            />
+          </div>
+          <div class="col-md-4">
+            <img
+              src="../../assets/css/img/product/pr1.png"
+              style="width: 150px; height: 150px"
+              alt=""
+            />
+          </div>
         </div>
-        <br>
-                <v-app id="inspire" style="width: 400px; height: 300px;">
+        <br />
+        <v-app id="inspire" style="width: 400px; height: 300px">
           <v-carousel cycle hide-delimiter-background :show-arrows="true">
             <v-carousel-item>
               <v-sheet height="auto">
                 <v-row class="fill-height" align="center" justify="center">
-                  <img src="../../assets/css/img/product/pr1.png" class="img-fluid" alt="" />
+                  <img
+                    src="../../assets/css/img/product/pr1.png"
+                    class="img-fluid"
+                    alt=""
+                  />
                 </v-row>
               </v-sheet>
             </v-carousel-item>
             <v-carousel-item>
               <v-sheet>
                 <v-row class="fill-height" align="center" justify="center">
-                  <img src="../../assets/css/img/product/pr2.png"  class="img-fluid"  alt="" />
+                  <img
+                    src="../../assets/css/img/product/pr2.png"
+                    class="img-fluid"
+                    alt=""
+                  />
                 </v-row>
               </v-sheet>
             </v-carousel-item>
             <v-carousel-item>
               <v-sheet>
                 <v-row class="fill-height" align="center" justify="center">
-                  <img src="../../assets/css/img/product/pr3.png"  class="img-fluid" alt="" />
+                  <img
+                    src="../../assets/css/img/product/pr3.png"
+                    class="img-fluid"
+                    alt=""
+                  />
                 </v-row>
               </v-sheet>
             </v-carousel-item>
@@ -53,6 +83,5 @@ export default {
 };
 </script>
 <style scoped>
-.detail {
-}
+
 </style>>
